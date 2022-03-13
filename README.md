09bb817ae12e4fcc9323b2337c61d14b576ce830
KevChaplin <kevchaplin44@gmail.com>
2022-03-13 16:26:56 +0900

    file post route functional

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# File Metadata Microservice
+
+## Project 5 of the Free Code Camp's Back End Development and APIs Course
+
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+// const mongoose = require('mongoose')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/'})
 
 var app = express();
+
+// Connect DB
+// const connectDB = (url) => {
+//   return mongoose.connect(url)
+// }
+
+// MIDDLEWARE
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+// ROUTES
+
+app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file)
+  const resObj = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  res.json(resObj)
+})
 
 
+// Basic Configuration
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+// Run App
+const start = () => {
+  try {
+    // await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => 
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+start()
